5141a77dc1316f7764036e741f7d715d207d5fbd
K. B. <151694873+k-bogdanov@users.noreply.github.com>
2026-01-08 13:36:46 +1300

    edit

--- R and bash pipelines/amplicon/1_import_and_rarefy.R
+++ R and bash pipelines/amplicon/1_import_and_rarefy.R
@@ -15,10 +15,10 @@ theme_norway <- theme_minimal() +
 set.seed(12358)
 
 # import ps objects
-Norway_16S_ps <- readRDS("C:/Users/bogda/Documents/PhD/TRUESOIL/molecular_data/2025/phyloseqs/ps_Norway_16S.rds") %>%
+Norway_16S_ps <- readRDS("~/ps_Norway_16S.rds") %>%
   name_na_taxa(na_label = "Unidentified <tax> (<rank>)")
 
-Norway_ITS_ps <- readRDS("C:/Users/bogda/Documents/PhD/TRUESOIL/molecular_data/2025/phyloseqs/ps_Norway_ITS.rds") %>%
+Norway_ITS_ps <- readRDS("~/ps_Norway_ITS.rds") %>%
   name_na_taxa(na_label = "Unidentified <tax> (<rank>)")
 
 
@@ -30,4 +30,5 @@ Norway_16S_ps_r <- rarefy_even_depth(Norway_16S_ps, 5000, rngseed = TRUE)
 Norway_ITS_ps_r <- rarefy_even_depth(Norway_ITS_ps, 5000, rngseed = TRUE)
 
 # pull metadata
+
 Norway_metadata <- data.frame(sample_data(Norway_16S_ps_r))
